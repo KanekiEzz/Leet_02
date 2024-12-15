bc5aae20439f16ad22ed239917afc89e9f09aac5
Ilyass Ezzam <iezzam@e1r3p15.1337.ma>
2024-12-15 17:43:52 +0100

    1337

---
+++ level_2/max/max.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int		max(int* tab, unsigned int len)
+{
+  int i = 0;
+  if (len == 0)
+    return i;
+  int result;
+  result = tab[i];
+  while (i < len)
+  {
+    if (result < tab[i])
+      result = tab[i];
+    i++;
+  }
+  return result;
+}
