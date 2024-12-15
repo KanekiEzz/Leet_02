15d3f44b59adf7cfe52e9709475087d03e1836d9
Ilyass Ezzam <iezzam@e1r3p15.1337.ma>
2024-12-15 18:08:59 +0100

    1337

---
+++ level_4/sort_list/sort_list.c
@@ -0,0 +1,75 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+t_list *sort_list(t_list *lst, int (*cmp)(int, int))
+{
+  int swap;
+  t_list *start;
+
+  start = lst;
+  while (lst != NULL && lst->next != NULL)
+  {
+    if ((*cmp)(lst->data, lst->next->data) == 0)
+    {
+      swap = lst->data;
+      lst->data = lst->next->data;
+      lst->next->data = swap;
+      lst = start;
+    }
+    else
+      lst = lst->next;
+  }
+  return start;
+}
+
+int ascending(int a, int b)
+{
+  return (a <= b);
+}
+
+t_list *create_node(int data)
+{
+    t_list *new_node = (t_list *)malloc(sizeof(t_list));
+    if (!new_node)
+        return NULL;
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void print_list(t_list *lst)
+{
+    while (lst)
+    {
+        printf("%d ", lst->data);
+        lst = lst->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    t_list *lst = create_node(4);
+    lst->next = create_node(3);
+    lst->next->next = create_node(1);
+    lst->next->next->next = create_node(2);
+
+    printf("Original list: \n");
+    print_list(lst);
+
+    lst = sort_list(lst, ascending);
+
+    printf("Sorted list: \n");
+    print_list(lst);
+
+    t_list *tmp;
+    while (lst)
+    {
+        tmp = lst;
+        lst = lst->next;
+        free(tmp);
+    }
+
+    return 0;
+}
