26189ed4a16991f8ed4cc9df1d2e5c8c0f40d3a2
! 𝕜𝕒𝕟𝕖𝕜𝕚 | かねき ⚡ <110631781+KanekiEzz@users.noreply.github.com>
2024-12-24 09:25:17 +0100

    Update sort_list.c

--- level_4/sort_list/sort_list.c
+++ level_4/sort_list/sort_list.c
@@ -5,26 +5,25 @@
 #include <unistd.h>
 #include "list.h"
 
+
 t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 {
-	int *swap;
+	t_list *start = lst;
 	int tmp;
 
-	tmp = lst;
-	while (lst->next != NULL)
+	while (lst != NULL && lst ->next != NULL)
 	{
-		if ((*cmp)(lst->data, lst->next->data))
+		if ((*cmp)(lst->data , lst->next->data) == 0)
 		{
 			tmp = lst->data;
 			lst->data = lst->next->data;
 			lst->next->data = tmp;
-			tmp = lst;
+			lst = start;
 		}
 		else
 			lst = lst->next;
 	}
-	tmp = lst;
-	return lst;
+	return start;
 }
 
 int ascending(int a, int b)
