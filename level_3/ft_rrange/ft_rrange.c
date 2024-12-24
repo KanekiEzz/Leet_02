190fe03a55e0f60f340e0622811ee24754c772d0
! 𝕜𝕒𝕟𝕖𝕜𝕚 | かねき ⚡ <110631781+KanekiEzz@users.noreply.github.com>
2024-12-24 10:41:02 +0100

    Update ft_rrange.c

--- level_3/ft_rrange/ft_rrange.c
+++ level_3/ft_rrange/ft_rrange.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *ft_rrange(int start, int end)
+int     *ft_rrange(int start, int end)
 {
-    int len;
-    int i = 0;
-
-    if (start > end)
-        len = start - end;
-    else if (end > start)
-        len = end - start;
-
-    int *ptr = malloc(len);
-    if (start > end)
-    {
-        while (i <= len)
-        {
-            ptr[i] = start--;
-            i++;
-        }
-    }
-    else
-    {
-        while (i <= len)
-        {
-            ptr[i] = end--;
-            i++;
-        }
-    }
-    return ptr;
+	int len;
+	if (start > end)
+		len = start - end;
+	else
+		len = end - start;
+	int *ptr = malloc(len * sizeof(int));
+	if (!ptr)
+		return NULL;
+	int i = 0;
+	if (start > end)
+	{
+		while (i <= len)
+		{
+			ptr[i] = end++;
+			i++;
+		}
+	}
+	else
+	{
+		while (i <= len)
+		{
+			ptr[i] = end--;
+			i++;
+		}
+	}
+	return ptr;
 }
